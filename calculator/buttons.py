90fc84e3bf9fc07abbb8159aa76fe163ed2a5ab1
Rodrigo Bruns <rodrigobrunsfh@gmail.com>
2023-12-04 13:33:00 -0300

    fix specialButtons and clear; add continuous eq

--- calculator/buttons.py
+++ calculator/buttons.py
@@ -84,6 +84,9 @@ class ButtonsGrid(QGridLayout):
                 self._makeSlot(self._operatorClicked, button)
                 )
             
+        if text == '=':
+            self._connectButtonClicked(button, self._eq)
+
     def _makeSlot(self, func, *args, **kwargs):
         @Slot(bool)
         def realSlot(_):
@@ -119,5 +122,24 @@ class ButtonsGrid(QGridLayout):
 
         self._op = buttonText
         self.equation = f'{self._left} {self._op} ???'
-            
+        
+    def _eq(self):
+        displayText = self.display.text()
+
+        if not isValidNumber(displayText):
+            return
+
+        self._right = float(displayText)
+        self.equation = f'{self._left} {self._op} {self._right}'
+        result = 0.0
+
+        try:
+            result = eval(self.equation)    
+        except ZeroDivisionError:
+            print('ZeroDivisionError')
+
+        self.display.clear()
+        self.info.setText(f'{self.equation} = {result}')
+        self._left = result
+        self._right = None
         
