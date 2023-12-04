8527e429e1a63a0618f10b3904a917696f2e0c5b
Rodrigo Bruns <rodrigobrunsfh@gmail.com>
2023-12-04 14:09:33 -0300

    add potentiation

--- calculator/buttons.py
+++ calculator/buttons.py
@@ -1,3 +1,4 @@
+import math
 from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QGridLayout, QPushButton
 from utils import isNumOrDot, isValidNumber
@@ -78,7 +79,7 @@ class ButtonsGrid(QGridLayout):
         if text == 'C':
             self._connectButtonClicked(button, self._clear)
 
-        if text in '/*-+':
+        if text in '/*-+^':
             self._connectButtonClicked(
                 button, 
                 self._makeSlot(self._operatorClicked, button)
@@ -116,13 +117,13 @@ class ButtonsGrid(QGridLayout):
 
         if not isValidNumber(displayText) and self._left is None:
             return
-        
+
         if self._left is None:
             self._left = float(displayText)
 
         self._op = buttonText
         self.equation = f'{self._left} {self._op} ???'
-        
+
     def _eq(self):
         displayText = self.display.text()
 
@@ -131,15 +132,22 @@ class ButtonsGrid(QGridLayout):
 
         self._right = float(displayText)
         self.equation = f'{self._left} {self._op} {self._right}'
-        result = 0.0
+        result = 'error'
 
         try:
-            result = eval(self.equation)    
+            if '^' in self.equation and isinstance(self._left, float):
+                result = math.pow(self._left, self._right)
+            else:
+                result = eval(self.equation)
         except ZeroDivisionError:
             print('ZeroDivisionError')
+        except OverflowError:
+            print('OverflowError')
 
         self.display.clear()
         self.info.setText(f'{self.equation} = {result}')
         self._left = result
         self._right = None
         
+        if result == 'error':
+            self._left = None
